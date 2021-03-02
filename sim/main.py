5beaaeeaa122d3ef7c34805a9420bc3dc12cd01b
Harsh Rana <hr23232323@gmail.com>
2021-03-01 19:01:31 -0500

    fixed up some project folders and structure

--- sim/main.py
+++ sim/main.py
@@ -1,4 +1,9 @@
+from util.processor import Processor
+from util.sampler import Sampler
+
 def main():
+    processor = Processor()
+    sampler = Sampler()
     print("Hello World")
 
 if __name__ == "__main__":
