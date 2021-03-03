c9024d958462587b9eca3298a9c6f7c14a5faf60
Harsh Rana <hr23232323@gmail.com>
2021-03-02 20:00:19 -0500

    data is being read by pandas

--- sim/util/processor.py
+++ sim/util/processor.py
@@ -1,16 +1,19 @@
 import pandas as pd
+import os
+
+DATA_DIR = os.getcwd() + "/data/"
 
 class Processor:
     def __init__(self):
-        self.data_base_path = "../data/"
+        pass
 
     def process_data(self, file_name):
-        file_path = self.generate
-        raw_data = pd.read_csv(file_name)
+        file_path = self.generate_filepath(file_name)
+        raw_data = pd.read_csv(file_path)
         return raw_data
 
     def generate_filepath(self, file_name):
-        return self.data_base_path  + file_name
+        return DATA_DIR  + file_name
 
 
     def generate_monthly_returns(self):
